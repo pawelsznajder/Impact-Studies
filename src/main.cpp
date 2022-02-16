0028768e309ac5033ac047108283b263ea0692b9
Pawel Sznajder <pawel.sznajder@ncbj.gov.pl>
2022-02-16 21:27:55 +0100

    inprove main so it works on many systems

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,11 @@
 #ifdef __linux__
     #if __GNUC__ < 9
     	    #if __GNUC__ < 6
-		 #include <boost/filesystem.hpp>
-           	 namespace fs = boost::filesystem; 
+
+    			#define __USE_BOOST__
+
+		 		#include <boost/filesystem.hpp>
+           	 	namespace fs = boost::filesystem; 
 	    #else
             	 #include <experimental/filesystem>
             	 namespace fs = std::experimental::filesystem;
@@ -53,20 +56,38 @@ int main(int argc, char* argv[]){
 		}
 
 		//loop over files
-//		for (const auto& dirEntry : fs::recursive_directory_iterator(fs::path(argv[i]))){
-		const boost::filesystem::recursive_directory_iterator end;
-		for(boost::filesystem::recursive_directory_iterator dirEntry(fs::path(argv[i])); dirEntry != end; dirEntry++){
-
-
+		#ifdef __USE_BOOST__
+			const fs::recursive_directory_iterator end;
+			for(fs::recursive_directory_iterator dirEntry(fs::path(argv[i])); dirEntry != end; dirEntry++){
+		#else
+			for(const auto& dirEntry : fs::recursive_directory_iterator(fs::path(argv[i]))){
+		#endif
+		
 			//skip directories
-			if(! fs::is_regular_file(dirEntry->status())) continue;
+			#ifdef __USE_BOOST__
+				 if(! fs::is_regular_file(dirEntry->status())) continue;
+			#else
+				 if(! fs::is_regular_file(dirEntry.status())) continue;
+			#endif
 
 			//txt
-			if(dirEntry->path().extension() == ".txt"){
+			#ifdef __USE_BOOST__
+				if(dirEntry->path().extension() == ".txt"){
+			#else
+				if(dirEntry.path().extension() == ".txt"){
+			#endif
+
+				//get path
+				#ifdef __USE_BOOST__
+					std::string path = dirEntry->path().string();
+				#else
+					std::string path = dirEntry.path().string();
+				#endif
 				
+			
 				//print
 				std::cout << __func__ << 
-					" info: reading: " << dirEntry->path() << std::endl;
+					" info: reading: " << path << std::endl;
 
 				//variables
 				std::pair<double, double> crossSection;
@@ -76,7 +97,7 @@ int main(int argc, char* argv[]){
 
 				//read to collect atributes ===============
 				{
-					HepMC3::ReaderAscii inputFile(dirEntry->path().string());
+					HepMC3::ReaderAscii inputFile(path);
 
 					//to check if RC sample
 					size_t lastParticleSize = 0;
@@ -122,7 +143,7 @@ int main(int argc, char* argv[]){
 
     			//read to process events ===============
     			{
-					HepMC3::ReaderAscii inputFile(dirEntry->path().string());
+					HepMC3::ReaderAscii inputFile(path);
 
 					//loop over events
 					for(;;){
